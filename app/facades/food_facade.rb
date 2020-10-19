17ca678515a0ef27d95d14073a18b887a147ff0d
Judith Pillado <judith.pillado@gmail.com>
2020-10-19 11:58:04 -0500

    Unfinished facade

---
+++ app/facades/food_facade.rb
@@ -0,0 +1,16 @@
+class FoodFacade
+
+  def self.foods
+    food_type = params[:q]
+
+    conn = Faraday.new(url: "https://api.nal.usda.gov")
+
+    response = conn.get("fdc/v1/foods/search?api_key=#{ENV["FOODS_API_KEY"]}&query=#{food_type}&pageSize=10")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @foods = json[:foods].map do |food|
+      Food.new(food)
+    end
+  end
+end
