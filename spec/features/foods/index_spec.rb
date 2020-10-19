f136c2d797803626e93375242ef485481e9d2b69
Judith Pillado <judith.pillado@gmail.com>
2020-10-19 10:46:14 -0500

    Fix spec with appropriate button names

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Search Page' do
+
+  it 'displays a list of food items that contain certain ingredient', :vcr do
+    visit '/'
+    fill_in :q, with: "sweet potatoes"
+    click_on 'Search'
+    expect(current_path).to eq(foods_path)
+    expect(page).to have_content('10 Results')
+    expect(page).to have_css(".food", count: 10)
+
+    within(first(".food")) do
+      expect(page).to have_css(".code")
+      code = find(".code").text
+      expect(code).to_not be_empty
+
+      expect(page).to have_css(".description")
+      description = find(".description").text
+      expect(description).to_not be_empty
+
+      expect(page).to have_css(".owner")
+      owner = find(".owner").text
+      expect(owner).to_not be_empty
+
+      expect(page).to have_css(".ingredients")
+      ingredients = find(".ingredients").text
+      expect(ingredients).to_not be_empty
+    end
+  end
+end
