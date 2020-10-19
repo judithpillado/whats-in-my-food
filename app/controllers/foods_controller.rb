982328220373e4671304a5e23182992bcad8cdc5
Judith Pillado <judith.pillado@gmail.com>
2020-10-19 11:46:36 -0500

    Hard code page size

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,13 @@
 class FoodsController < ApplicationController
   def index
-    
+    food_type = params[:q]
+
+    conn = Faraday.new(url: "https://api.nal.usda.gov")
+
+    response = conn.get("fdc/v1/foods/search?api_key=#{ENV["FOODS_API_KEY"]}&query=#{food_type}&pageSize=10")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @foods = json[:foods]
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data('<FOOD_API_KEY>') { ENV['FOOD_API_KEY'] }
+  config.filter_sensitive_data('<FOODS_API_KEY>') { ENV['FOODS_API_KEY'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
